f27e73b63bf8880b1a907807ba198aa3596b9f12
weishengwx <2234346829@qq.com>
2022-11-20 16:30:55 +0800

    Initial commit

---
+++ id-gen-spring-boot-autoconfigure/src/main/java/com/wx/autoconfigure/IdGenAutoConfiguration.java
@@ -0,0 +1,6 @@
+package com.wx.autoconfigure;/**
+ * @author 22343
+ * @date 2022/11/18 8:06
+ * @version 1.0
+ */public class IdGenAutoConfiguration {
+}
---
+++ id-gen-spring-boot-autoconfigure/src/main/java/com/wx/autoconfigure/IdGenerateCommon.java
@@ -0,0 +1,6 @@
+package com.wx.autoconfigure;/**
+ * @author 22343
+ * @date 2022/11/19 1:35
+ * @version 1.0
+ */public class IdGenerateCommon {
+}
